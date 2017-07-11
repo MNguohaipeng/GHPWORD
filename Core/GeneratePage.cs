a68abdbfa6d17e3444430005e1a0781aea41c139
123 <12345@.qqcom>
2017-07-11 17:15:35 +0800

    2017年7月11日

--- Core/GeneratePage.cs
+++ Core/GeneratePage.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-
+using SqlSugar;
 namespace Core
 {
     public class GeneratePage
@@ -20,7 +20,7 @@ namespace Core
 
                 try
                 {
-                    var data = db.Queryable<Entity.PageUrl>().Where(T => T.IsDeleted == false).ToList();
+                    var data = db.Queryable<Entity.PageUrl>().Where(T => T.IsDeleted == false).ToList();//获取所有生成的页面信息
 
                     for (int i = 0; i < data.Count; i++)
                     {
@@ -29,38 +29,89 @@ namespace Core
                         {
                             Directory.CreateDirectory(ProjectFileUrl + data[i].HtmlUrl);
                         }
+                        if (Directory.Exists(ProjectFileUrl + data[i].ControllerUrl) == false)//如果不存在就创建file文件夹
+                        {
+                            Directory.CreateDirectory(ProjectFileUrl + data[i].ControllerUrl);
+                        }
 
-                        using (StreamWriter sw = new StreamWriter(ProjectFileUrl + data[i].HtmlUrl + "/Add.cshtml", false))
+                        string entityName = data[i].EntityName;
+
+                        #region 创建html
+                        using (StreamWriter sw = new StreamWriter(ProjectFileUrl + data[i].HtmlUrl + "/Add.cshtml", false, Encoding.UTF8))//创建文件
                         {
 
-                            Type type = data[i].GetType();
 
-                            //再用Type.GetProperties获得PropertyInfo[],然后就可以用foreach 遍历了
-                            foreach (PropertyInfo pi in type.GetProperties())
+
+                            var TableString = db.Queryable<Entity.TableString>().Where(T => T.IsDeleted == false && T.TableName == entityName).ToList();
+
+                            if (TableString != null)
                             {
-                                string name = pi.GetValue(data[i], null).GetType().Name;
 
-                                object value1 = pi.GetValue(data[i], null);//用pi.GetValue获得值
-           
-                               var one=db.Queryable<Entity.EditPage>().Where(T => T.IsDeleted == false&&T.YSName== name).First();
-                                if (one == null)
+                                string input = "";
+
+
+                                foreach (var item in TableString)
                                 {
-                                    //throw new Exception("没有对应的数据类型");
-                                }
-                                else {
-                                    sw.Write(one.YSValue);
+                                    var one = db.Queryable<Entity.EditPage>().Where(T => T.IsDeleted == false && T.YSName == item.InputType).First();
+                                    if (one == null)
+                                    {
+                                        //throw new Exception("没有对应的数据类型");
+                                    }
+                                    else
+                                    {
+                                        input = string.Format(one.YSValue, item.FieldShowMing, item.FieldName);
+
+                                    }
                                 }
 
+
+                                //读取模板
+                                string cshtml = File.ReadAllText(ProjectFileUrl + @"\Models\cshtml模版.txt");
+
+                                string PageName = data[i].PageName;
+
+                                string title = "@{Layout = \"~/Views/Shared/_Layout.cshtml\";}";
+
+                                string AddHtml = string.Format(cshtml, PageName, entityName + "AddForm", input, title);
+
+                                byte[] bs = Encoding.UTF8.GetBytes(AddHtml);
+                                AddHtml = Encoding.UTF8.GetString(bs);
+
+                                sw.Write(AddHtml);
 
 
                             }
                         }
+                        #endregion
+
+
+                        #region 创建控制器
+                        using (StreamWriter zs = new StreamWriter(ProjectFileUrl + data[i].ControllerUrl + "/" + entityName + "Controller.cs", false, Encoding.UTF8))//创建文件
+                        {
+
+
+
+
+
+
+                            //读取模板
+                            //string controllertxt = string.Format(File.ReadAllText(ProjectFileUrl + @"\Models\Controller模板.txt", Encoding.UTF8), entityName);
+                            using (StreamReader sr = new StreamReader(ProjectFileUrl + @"\Models\Controller模板.txt"))
+                            {
+                                string msg=sr.ReadToEnd();
+                                msg=msg.Replace("{0}", entityName);
+                                zs.Write(msg);
+                            }
+
+
+
+
+
+
+                            #endregion
+
+                        }
                     }
-
-
-
-
-
 
                 }
                 catch (Exception ex)
---
+++ Entity/TableString.cs
@@ -0,0 +1,41 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+
+
+   /*创建时间：
+    * 2017年7月11日09:21:12
+    * 创建人 : 
+    * 郭海鹏
+    * 说明 :
+    * 生成页面 和 控制器的核心代码类
+    */
+
+  public class TableString:B_EntityBase
+    {
+        [SugarColumn(Length = 50)]
+        public string TableName { get; set; }
+        [SugarColumn(Length = 50)]
+        public string FieldName { get; set; }
+        [SugarColumn(Length = 50)]
+        public string FieldShowMing { get; set; }
+        [SugarColumn(Length = 50)]
+        public string InputType { get; set; }
+        [SugarColumn(IsNullable =true,Length = 500)]
+        public string AddWhere { get; set; }  //添加时的筛选条件 用逗号分割  不能为空等等~~~
+
+        public bool IsOtherTable { get; set; } //是否关联其他表数据
+        [SugarColumn(IsNullable = true, Length = 50)]
+        public string OtherTableName { get; set; }//其他表名称  
+        [SugarColumn(IsNullable = true, Length = 500)]
+        public string OtherTableFieldZS { get; set; }//关联其他表的字段展示用  逗号分割 
+        [SugarColumn(IsNullable = true, Length = 50)]
+        public string OtherTableFieldBC { get; set; }//关联其他表的字段保存用
+    }
+}
